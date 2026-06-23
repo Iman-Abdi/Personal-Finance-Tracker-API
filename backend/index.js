17ea9eaebe91162238e03b20cf513aef065044c5
Iman-Abdi <leofaragaab1@gmail.com>
2026-06-23 11:28:43 +0300

    changing index.js

--- backend/index.js
+++ backend/index.js
@@ -29,8 +29,10 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  // Catch-all route for SPA fallback
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+
   });
 }
 
